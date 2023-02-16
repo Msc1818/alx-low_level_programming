bb357686224fd094ae4a3f0e623202b3344d2f7a
Msc1818 <chairi1818@gmail.com>
2023-02-16 07:50:24 -0800

    auto commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that print the size of char and others
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("Size of a char:1 byte(s)\n");
+printf("Size of an int:4 byte(s)\n");
+printf("Size of a long int: 4 byte(s)\n");
+printf("Size of a long long int: 8 byte(s)\n");
+printf("Size of a float: 4 byte(s)\n");
+return (0);
+}
