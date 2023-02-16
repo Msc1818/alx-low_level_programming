077a9e442e7837691290760c87cc4b2bf6331bfd
Msc1818 <chairi1818@gmail.com>
2023-02-16 06:54:05 -0800

    auto commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - program that print programming is like building a multilingual puzzle with puts fonction \n
- *
- * return: 0 (succes)
+ * main - Entry point
+ * program that uses a puts fonction
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+return (0);
 }
-
