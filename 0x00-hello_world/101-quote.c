bdfb3dd2d4c76ea19d9d68796f41e0b231caf543
Msc1818 <chairi1818@gmail.com>
2023-02-16 10:45:46 -0800

    auto commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints out  the standard error.
+ * main - Prints out  the standard error
  * Return: 1 (success)
  */
 int main(void)
 {
-        write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
