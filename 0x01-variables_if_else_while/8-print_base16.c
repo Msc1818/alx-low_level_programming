9fbedea6acfcc0472cd5bfeb4a6bc27db5e8eb5d
Msc1818 <chairi1818@gmail.com>
2023-02-17 10:03:52 -0800

    auto commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - main block
+* Description: print 0123456789abcdef
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+	char f = '0';
+
+	for (f = 0; f <= '9'; f++)
+
+		putchar(d);
+
+	for (c = 'a'; c <= 'f'; c++)
+
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
