9cbe56dec2daa6dded5751ac7dd6700315796b41
Msc1818 <chairi1818@gmail.com>
2023-02-17 09:42:48 -0800

    auto commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+* main - Entry block
+* Description: putchar  single dicimal  numbers
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c++)
+	putchar (c);
+	putchar('\n');
+	return (0);
+}
