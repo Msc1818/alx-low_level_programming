21471ef98457e98f660edc7f0227d58d1007991d
Msc1818 <chairi1818@gmail.com>
2023-02-17 07:39:00 -0800

    auto commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - entry block
- * Description: alphabets printed in lower and upper
- * Return: 0
- */
+* main - entry block
+* Description: alphabets printed in lower and upper
+* Return: 0
+*/
 int main(void)
 {
-        char c , m ;
-        c = 'a';
-        m = 'A';
-        while (c <= 'z')
-        {
-                putchar(c);
-                c++;
-        }
-        while (m <= 'Z')
-        {
-                putchar(m);
-                m++;
-        }
-        putchar('\n');
-        return (0);
+	char c, m;
+
+	c = 'a';
+	m = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c)
+		c++;
+	}
+	while (m <= 'Z')
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
 }
