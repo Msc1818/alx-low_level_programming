343b89723101bfb814145e5140596441615265d4
Msc1818 <chairi1818@gmail.com>
2023-02-17 10:34:03 -0800

    auto commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,22 +2,25 @@
 #include <stdlib.h>
 /**
 * main - main block
-* Description: print 0123456789abcdef
+* Description: prints 0_to_9 then a_to_f
 * Return: 0
 */
 int main(void)
 {
 	char c;
-	char f = '0';
+	char x = '0';
 
-	for (f = 0; f <= '9'; f++)
-
-		putchar(f);
+	while (x <= '9')
+	{
+		putchar(x);
+		x++;
+	}
 
 
 	for (c = 'a'; c <= 'f'; c++)
-
+	{
 		putchar(c);
+	}
 
 	putchar('\n');
 	return (0);
