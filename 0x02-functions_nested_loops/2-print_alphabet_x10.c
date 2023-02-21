859e6998da097d86b118cbaf81a654ff2e9f860c
Msc1818 <chairi1818@gmail.com>
2023-02-21 10:19:10 -0800

    auto commit by script git

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+	* print_alphabet_x10 - print alphabet inlowercas x10
+	* first task print _putchar
+	* Return always (0) (success)
+*/
+	void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int f;
+
+	for (f = 0; f < 10; f++)
+	while (c < 'z')
+	{
+		_putchar('c');
+		c++;
+	}
+	_putchar('\n');
+}
