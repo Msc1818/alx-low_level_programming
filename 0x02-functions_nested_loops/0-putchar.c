2146bf0e38449223347ed2ab7a0062c5c71a24d1
Msc1818 <chairi1818@gmail.com>
2023-02-21 09:20:49 -0800

    auto commit by script git

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
 #include "main.h"
 
 /**
- * main - print _putchar
- * first task print _putchar
- * Return always (0) (success)
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
