82d2bba9356d6c6bfed4ea37c132a27cc59c050f
Msc1818 <chairi1818@gmail.com>
2023-02-21 12:55:05 -0800

    auto commit by script git

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
+ * sumof 3 and 5 from 0 to 1024
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int h, sum = 0;
 
-	for (h = 0; i < 1024; h++)
+	for (h = 0; h < 1024; h++)
 	{
 		if ((h % 5) == 0 || (h % 3) == 0)
 			sum += h;
