f8dbe660fde310788a91ad3cdcd4ef74a2a48e35
Msc1818 <chairi1818@gmail.com>
2023-02-21 12:04:28 -0800

    auto commit by script git

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - Check Main
+ * @r: An absolute value of the nuber r
+ * This pr returns absolute value of a number
+ * Return: Absolut value of number r
+ */
+int _abs(int r)
+{
+	if (r <= 0)
+	return (r * -1);
+	else
+		return (r);
+}
