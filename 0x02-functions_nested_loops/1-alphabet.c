92a57a45d9e59871c7a53b810e3df74fab400b4c
Msc1818 <chairi1818@gmail.com>
2023-02-21 09:43:35 -0800

    auto commit by script git

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
 #include "main.h"
 /**
 *print_alphabet-prints the alphabet,in lowercase+\n
+*
 *Return: no return value
 */
 	void print_alphabet(void)
 	{
 		char c = 'a';
+
 		while (c <= 'z')
-{
-_putchar(c);
-}
+		{
+			_putchar(c);
+			c++
+		}
 		_putchar('\n');
 	}
