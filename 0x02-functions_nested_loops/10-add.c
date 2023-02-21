7aeeccda40c9d43e182c580d047de74b6f1f91c5
Msc1818 <chairi1818@gmail.com>
2023-02-21 12:28:30 -0800

    auto commit by script git

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Check Main
+ * @c: integer c
+ * @d: integer d
+ * Function adds two integers
+ * Return: Result to add c and d
+ */
+int add(int c, int d)
+{
+	return (c + d);
+}
