7362241f8b73d2cf04db4bfdb26f19529462d2b6
Msc1818 <chairi1818@gmail.com>
2023-02-21 12:59:43 -0800

    auto commit by script git

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * print the first 50 fibonnaci number 
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* print the first 50 fibonnaci number
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int i;
