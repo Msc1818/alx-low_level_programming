2f9323b57eecf497e01deedb16654ff22ad56878
Msc1818 <chairi1818@gmail.com>
2023-02-27 04:53:24 -0800

    auto update by script

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* puts_half - Prints half of a string
+*@str: string to print
+*Return: 0 (success)
+*/
+void puts_half(char *str)
+{
+	int i, j, l;
+
+	len = 0;
+	while (*(str + j) != '\0')
+		j++;
+	l = j;
+	j = j / 2;
+	if (l % 2 != 0)
+	{
+		j++;
+	}
+	for (i = 0; i < l; i++)
+	{
+		if (i >= j)
+			_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
