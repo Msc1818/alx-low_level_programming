a21579409163c7a9403b2e90b3eb714de33838ba
Msc1818 <chairi1818@gmail.com>
2023-02-27 04:21:48 -0800

    auto update by script

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of a string
+ */
+void _puts(char *str)
+{
+	int c = 0;
+
+	while (str[c] != '\0')
+	{
+		_putchar(str[c]);
+		c++;
+	}
+	_putchar('\n');
+}
